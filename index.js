f5640f7bf687884829f083a57bfec194be19d8f7
Gustavo Cremonez <aparecido2332001@gmail.com>
2022-09-07 16:22:45 -0300

    inclusão de novos pacotes

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const path = require('path');
 const routes = require('./src/routes/router');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
 
 const app = express();
+const db = require('./src/database/connection');
+
+db.connect();
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'views')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.urlencoded({  extended: true }));
+app.use(express.urlencoded({  extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 app.use('/', routes);
 
 
