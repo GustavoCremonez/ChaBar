4b54ff285e1c2d83a7bdf0c414b4462420fe1c9b
Gustavo Cremonez <aparecido2332001@gmail.com>
2022-09-07 16:39:05 -0300

    Adicionando controle para confirmação e cadastro da presença

--- src/Controller/PresencaController.js
+++ src/Controller/PresencaController.js
@@ -1,11 +1,22 @@
+const Presenca = require('../model/Presencas');
 
 function  ConfirmarPresenca(req, res){
-  const form = req.body;
+  const Nome = req.body.nome;
+  const Acompanhantes = req.body.acompanhantes;
   
+  const presenca = new Presenca({Nome, Acompanhantes});
 
-  res
-  .status(200)
-  .redirect('/Presenca')
+  presenca.save()
+    .then(() => {
+      res
+      .status(200)
+      .redirect('/Presenca');
+    })
+    .catch((error) => {
+      res
+      .status(500)
+      .redirect('/Presenca');
+    })
 }
 
 module.exports = {
