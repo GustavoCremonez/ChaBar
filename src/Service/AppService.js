635d6c9ab6877495000479aef44466d752544934
Gustavo Cremonez <aparecido2332001@gmail.com>
2022-11-07 19:31:10 -0300

    Att validacao

--- src/Service/AppService.js
+++ src/Service/AppService.js
@@ -1,52 +1,68 @@
 const PresencaController = require('../Controller/PresencaController');
-const PrecisamosController = require('../Controller/PrecisamosController')
+const PrecisamosController = require('../Controller/PrecisamosController');
 
-async function PegueItensFaltantes(){
-  const confirmados = await PresencaController.PresentesConfirmados();
+async function PegueItensFaltantes() {
+	const confirmados = await PresencaController.PresentesConfirmados();
 
-  const itensFaltantes = await PrecisamosController.ItensQueNaoTemos(confirmados);
+	const itensFaltantes = await PrecisamosController.ItensQueNaoTemos(
+		confirmados,
+	);
 
-  return itensFaltantes
+	return itensFaltantes;
 }
 
-async function CadastrarPresenca(req, res) { 
-  const Nome = req.body.nome, 
-        Acompanhantes  = req.body.acompanhantes,
-        Presente = req.body.presente;
+async function CadastrarPresenca(req, res) {
+	const Nome = req.body.nome,
+		Acompanhantes = req.body.acompanhantes,
+		Presente = req.body.presente;
 
-  const itens = await PegueItensFaltantes();
+	const itens = await PegueItensFaltantes();
 
-  console.log(Presente)
-  if(!Nome) {
-    return res.render('Presenca', {presencaMarcada: false, itens, validacao: {valor: true, campo: 'nome'}})
-    }
+	if (!Nome) {
+		return res.render('Presenca', {
+			presencaMarcada: false,
+			itens,
+			validacao: { valor: true, campo: 'nome' },
+		});
+	}
 
-  if(!Acompanhantes || isNaN(Acompanhantes)){
-      return res.render('Presenca', {presencaMarcada: false, itens, validacao: {valor: true, campo: 'Acompanhantes'}})
-    }
-  if(Presente === 'Selecione um presente'){
-    return res.render('Presenca', {presencaMarcada: false, itens, validacao: {valor: true, campo: 'Presente'}})
-  }
+	if (!Acompanhantes || isNaN(Acompanhantes)) {
+		return res.render('Presenca', {
+			presencaMarcada: false,
+			itens,
+			validacao: { valor: true, campo: 'Acompanhantes' },
+		});
+	}
+	if (Presente === 'Selecione um presente') {
+		return res.render('Presenca', {
+			presencaMarcada: false,
+			itens,
+			validacao: { valor: true, campo: 'Presente' },
+		});
+	}
 
-  
-  const nomePresente = await PrecisamosController.PegaNomePresente(Presente);
+	const nomePresente = await PrecisamosController.PegaNomePresente(Presente);
 
-  let Entidade = {
-    Nome,
-    Acompanhantes,
-    Presente: nomePresente.Presente 
-  };
+	let Entidade = {
+		Nome,
+		Acompanhantes,
+		Presente: nomePresente.Presente,
+	};
 
-  PresencaController.ConfirmarPresenca(Entidade)
-  .then(async () => {
-    res.render('Presenca', {presencaMarcada: true, itens, validacao:{valor: false, campo: ''}})
-  })
-  .catch((error) => {
-    throw(error);
-  })
+	PresencaController.ConfirmarPresenca(Entidade)
+		.then(async () => {
+			res.render('Presenca', {
+				presencaMarcada: true,
+				itens,
+				validacao: { valor: false, campo: '' },
+			});
+		})
+		.catch((error) => {
+			throw error;
+		});
 }
 
 module.exports = {
-  PegueItensFaltantes,
-  CadastrarPresenca
-}
+	PegueItensFaltantes,
+	CadastrarPresenca,
+};
